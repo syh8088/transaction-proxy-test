83ff8d9f7f54dae99c8806fa09aee791c8c0adc0
SeoYangHun <syh8088@gmail.com>
2021-11-29 16:10:18 +0900

    프로젝트 생성

---
+++ src/main/java/com/transactionproxytest/TransactionProxyTestApplication.java
@@ -0,0 +1,13 @@
+package com.transactionproxytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionProxyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionProxyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transactionproxytest/TransactionProxyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactionproxytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionProxyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
